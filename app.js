4276172f26ab36669012cfe5e0f8baa11ceacc6a
Abdulaziz Hamid <abdulaziz5536h@gmail.com>
2025-12-20 20:00:39 +0300

    update app js

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const sentence = 'my name is abdulaziz';
-const vowels = ['a','e','i','o','u'];
-const match = [];
-let i,j;
-for(i=0;i<sentence.length;i++){
-  for(j=0;j<vowels.length;j++){
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at several points along the way to take photos, but I never stopped riding. The scenery was absolutely beautiful, the trail was very well maintained, and the people I met along the way were extremely friendly. I went to a really cool cafe in Nyack, where I had a really delicious bagel. I loved the whole experience, and I will definitely be doing this again next weekend!';
 
-    if(sentence[i]==='e'||sentence[i]==='u'){
-      match.push(sentence[i]);
-    }
-    if(sentence[i] === vowels[j]){
-      match.push(sentence[i])
-    }
+let storyWords = story.split(' ');
 
+
+
+console.log(storyWords.length);
+
+
+
+storyWords = storyWords.filter(words => words !== 'literally')
+
+storyWords = storyWords.map(words => {
+  if(words==='beautifull'){
+    return 'beautiful'
   }
-}
-console.log(match.join('').toUpperCase());
+  return words;
+})
+
+const overUsedWords = ['really', 'very', 'basically'];
+
+storyWords = storyWords.filter(words => overUsedWords.includes(words));
 
 
-
-
-
-
-
+console.log(storyWords);
 
 
 
