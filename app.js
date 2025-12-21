adeacec1588c1ba8d78eb7795d20d2252ded238c
Abdulaziz Hamid <abdulaziz5536h@gmail.com>
2025-12-21 19:35:41 +0300

    commit

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
-let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at several points along the way to take photos, but I never stopped riding. The scenery was absolutely beautiful, the trail was very well maintained, and the people I met along the way were extremely friendly. I went to a really cool cafe in Nyack, where I had a really delicious bagel. I loved the whole experience, and I will definitely be doing this again next weekend!';
+const student = {
 
-let storyWords = story.split(' ');
-
-
-
-console.log(storyWords.length);
-
-
-
-storyWords = storyWords.filter(words => words !== 'literally')
-
-storyWords = storyWords.map(words => {
-  if(words==='beautifull'){
-    return 'beautiful'
+  name:'Abdulaziz',
+  grade:90,
+  subjects:['math','english'],
+  introduce() {
+    console.log('hi my name is Abdulaziz');
   }
-  return words;
-})
+}
 
-const overUsedWords = ['really', 'very', 'basically'];
+student.grade = 80;
+student.isGraduated = false;
 
-storyWords = storyWords.filter(words => overUsedWords.includes(words));
+for(let looper in student){
 
-
-console.log(storyWords);
-
-
+  console.log(looper,student[looper]);
+  
+}
 
 
