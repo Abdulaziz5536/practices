d959b86b147a67417c52a2ce87ae2a9e2d8798b9
Abdulaziz Hamid <abdulaziz5536h@gmail.com>
2026-01-20 18:57:14 +0300

    commit

--- app.js
+++ app.js
@@ -1,126 +1,41 @@
-const delayMessage = () => {
+const username = (input) => {
   return new Promise((resolve) => {
-    setTimeout(()=>{
-      resolve('Hello after 1 second');
-    },1000)
-  })
-}
-
-delayMessage()
-.then(msg => console.log(msg));
-
-
-const doubleNumber = (num) => {
-  return new Promise((resolve) => {
-    resolve(num * 2);
-  })
-}
-doubleNumber(5)
-.then(value => console.log(value));
-
-
-const checkAge = (age) => {
-  return new Promise((resolve,reject) => {
-    if(age>=18){
-      resolve('Access granted!');
-    }
-    else if(age<18){
-      reject('Access denied!');
-    }
-  })
-
-}
-
-checkAge(22)
-.then(value => console.log(value))
-.catch(value => console.log(value));
-
-
-const addFive = (num) => {
-  return new Promise((resolve) => {
-    resolve(num + 5);
-  })
-}
-addFive(10)
-.then(value => addFive(value))
-.then(value => console.log(value));
-
-
-const fetchData = () => {
-  return new Promise((resolve)=>{
-    setTimeout(() => {
-      resolve('Data recieved!');
-    },2000);
-  })
-}
-fetchData()
-.then(msg => console.log(msg));
-
-
-const divide = (a, b) => {
-  return new Promise((resolve, reject) => {
-    if (b === 0) reject("Cannot divide by zero");
-    else resolve(a / b);
+    resolve(input);
   });
-};
-divide(10,0)
-.then(result => console.log(result))
-.catch(result => console.log(result));
 
-
-firstPromise = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('First');
-  },1000);
-})
-
-secondPromise = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('second')
-  },2000);
-})
-Promise.all([firstPromise,secondPromise])
-.then(result => console.log(result));
-
-
-const loginUsername = (userName) => {
-  return new Promise((resolve,reject) => {
-    if(userName === 'admin'){
-      resolve('welcome');
-    }
-    else{
-      reject('invalid user');
-    }
-  })
 }
-loginUsername('admin').then(value => console.log(value))
-.catch(err => console.log(err));
 
-
-const checkStock = () => {
+const password = (user,password) => {
   return new Promise((resolve) => {
-    setTimeout(() => {
-       resolve('stock available');
-   },1000)
+    resolve(user,password);
   })
 }
 
-const processPayment = () => {
+const session = (user) => {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('payment processed')
-    },1000)
+    resolve(user);
   })
 }
 
-const shipOrder = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('order shipped');
-    },1000)
-  })
+const getUser = async () => {
+  let user = await username("Abdulaziz");
+  console.log(user);
 }
-checkStock().then(msg => console.log(msg));
-processPayment().then(msg => console.log(msg));
-shipOrder().then(msg => console.log(msg));
-console.log('order completed');
+const checkPassword = async () => {
+  let pswCheck = await password(['Abdulaziz',1234]);
+  console.log(pswCheck);
+}
+const createSession = async () => {
+  let ss = await session('1');
+  console.log(ss);
+  
+
+}
+
+username();
+password();
+session();
+
+getUser();
+checkPassword();
+createSession();
